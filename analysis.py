cc2a65a925548fe2eb6ea5b5bf5b57519f22e8ed
MisaghMomeniB <jenabemisagh@gmail.com>
2024-11-09 18:01:04 +0330

    Initial data overview

--- analysis.py
+++ analysis.py
@@ -5,3 +5,9 @@ import seaborn as sns
 # Reading the CSV File
 file_path = 'weather-csv.csv'
 data = pd.read_csv(file_path)
+
+# Initial data overview
+print("Initial Data Information : ")
+print(data.info())
+print("\nStatistical Summary of Data : ")
+print(data.describe())
